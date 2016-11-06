d63129dbe37d6cc2bcc718b020b2ba4c7ae29b81
Nick Rance <nicholas.rance@gmail.com>
2016-11-06 01:16:07 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+/**
+ * Created by nick-dev on 11/5/16.
+ * This file sends the drone log info via termix commands
+ */
+var server_url="http://nranceedd.azurewebsites.net/";
+var io = require('socket.io-client')(server_url);
+var exec = require('child_process').exec;
+var child;
+//var request = require('request');
+
+function fivesec() {
+    setInterval(function () {
+        console.log('5 second passed');
+        io.emit('drone_message', "5 seconds have passed");
+    }, 5000);
+}
+
+// child = exec("echo 'This is being called from my node script'",function (error, stdout, stderr) {
+//     console.log('stdout: ' + stdout);
+//     console.log('stderr: ' + stderr);
+//     if (error !== null) {
+//         console.log('exec error: ' + error);
+//     }
+// });
+
+io.on('connect', function onConnect(){
+    console.log('Client has connected');
+    fivesec();
+});
