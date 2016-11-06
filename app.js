3f44decf1d18ab03c83e6fc910fbb47aaaae4aca
Nick Rance <nicholas.rance@gmail.com>
2016-11-06 16:05:29 -0500

    Responds to client commands

--- app.js
+++ app.js
@@ -5,29 +5,42 @@
 var server_url="http://nranceedd.azurewebsites.net/";
 var io = require('socket.io-client')(server_url);
 const exec = require('child_process').exec;
-const cmd = 'termux-battery-status';
+var cmd = 'termux-battery-status';
 var child;
 
-function fivesec() {
-    setInterval(function () {
-            exec(cmd, function(error, stdout, stderr) {
-                if (error) {
-                    io.emit('drone_message',"Error: "+ error);
-                }
-                else{
-                    io.emit('drone_message', "Output: " +stdout);
-                }
-            });
+// function fivesec() {
+//     setInterval(function () {
+//             exec(cmd, function(error, stdout, stderr) {
+//                 if (error) {
+//                     io.emit('drone_message',"Error: "+ error);
+//                 }
+//                 else{
+//                     io.emit('drone_message', "Output: " +stdout);
+//                 }
+//             });
+//
+//         }
+//         , 5000);
+// }
 
+function run_cmd(command) {
+    exec(command, function (error, stdout, stderr) {
+        if (error) {
+            io.emit('drone_message', "Error: " + error);
         }
-        , 5000);
+        else {
+            io.emit('drone_message', "Output: " + stdout);
+        }
+    });
 }
 
 io.on('connect', function(socket){
     console.log('Client has connected');
-     fivesec();
+     //fivesec();
      io.on('client_message', function(msg){
-         console.log(msg);
+         console.log("message received");
+         cmd = msg.replace("Client: ","");
+         run_cmd(cmd);
      });
       io.on('drone_message', function(msg){
           console.log(msg);
